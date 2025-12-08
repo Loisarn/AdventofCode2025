60f57acecc452d90ebf8212ac7a08e7eab3d2779
Louice Borg <louiceborg@hotmail.se>
2025-12-08 09:51:43 +0100

    day 1, part two

--- AdventOfCode2025/Day1/Day01.cs
+++ AdventOfCode2025/Day1/Day01.cs
@@ -11,30 +11,24 @@ public static class Day1
         var lines = File.ReadAllLines("Day1/input.txt");
 
         int position = 50;
-        int zeroCount = 0;
+        long zeroCount = 0;
 
         foreach (var line in lines)
         {
-            if (string.IsNullOrWhiteSpace(line))
-            continue;
-
             char dir = line[0];
             int amount = int.Parse(line[1..]);
 
-            if (dir == 'R')
+           for (int i = 0; i < amount; i++)
             {
-                position = (position + amount) % 100;
-            }
-            else if (dir == 'L')
-            {
-                position = (position - amount % 100 + 100) % 100;
-            }
+                position = dir == 'R'
+                ? (position + 1) % 100 :
+                (position - 1 + 100) % 100;
 
-            if (position == 0)
-            {
+                if (position == 0)
                 zeroCount++;
             }
-            Console.WriteLine($"The password is: {zeroCount}");
         }
+
+        Console.WriteLine($"Part two: {zeroCount}");
     }
 }
